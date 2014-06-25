51cca8976bb353b8ee2713d0ca13aea306dd7664
James Glasgow <james@bt.amahi.net>
2014-06-25 06:48:20 -0600

    fixed the quoting of the commands to allow for different characters in the file names and switched to using ffmpeg's builtin faststart option instead of qtfaststart

--- video_convert.py
+++ video_convert.py
@@ -1,4 +1,4 @@
-#! /bin/env python
+#! /usr/bin/env python
 
 import os, sys
 
@@ -8,22 +8,22 @@ def convert_file(orig_file_name):
   print "Starting to convert %s..." % orig_file_name
   new_file_name = orig_file_name.replace(orig_file_ext, 'mp4')
   # create the command to convert the file
-  command = "ffmpeg -i %s -acodec aac -strict -2 -ab 128k -ar 44100 -vcodec h264 -b 800K %s -y" % (orig_file_name, new_file_name)
+  command = 'ffmpeg -i "%s" -acodec aac -strict -2 -ab 128k -ar 44100 -vcodec h264 -b:v 800K -y -movflags +faststart "%s" ' % (orig_file_name, new_file_name)
   command = "time %s" % command
   # convert the file
   exit_code = os.system(command)
 
   if (exit_code == 0):
     # use qtfaststart to make the new file streaming ready
-    os.system("qtfaststart %s" % (new_file_name))
+    #os.system("qtfaststart %s" % (new_file_name))
     # move the original file
     orig_file_new_name = orig_file_name.replace('.%s' % orig_file_ext, '_%s' % orig_file_ext)
-    move_command = 'mv %s %s' % (orig_file_name, orig_file_new_name)
+    move_command = 'mv "%s" "%s"' % (orig_file_name, orig_file_new_name)
     os.system(move_command)
     print "\n"
     print "%s created and original file moved to %s" % (new_file_name, orig_file_new_name)
   else:
-    os.system("rm %s" % (new_file_name))
+    os.system('rm "%s"' % (new_file_name))
     print "Converting %s failed, please try again." % orig_file_name
   pass
 
